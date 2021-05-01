0c51a5efd8454050f6adf42122f88f686159e702
TheDe-bugger <jaimildalwadi2003@gmail.com>
2021-05-01 19:09:29 -0400

    Update ml.js

--- assets/ml.js
+++ assets/ml.js
@@ -1,22 +1,4 @@
-/*var request = new XMLHttpRequest()
-
-request.open('POST', 'https://vision.googleapis.com/v1/images:annotate', true)
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach((movie) => {
-      console.log(movie.title)
-    })
-  } else {
-    console.log('error')
-  }
-}
-
-request.send()
-*/
-
+//
 function ml(data){
 fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBkKPvGy2_fdq_GHe0uJmQVmJEs8ezNrPw', {
   method: 'POST', // or 'PUT'
